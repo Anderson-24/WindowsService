c3d942e513f3f1cc6661cbff1118ab5a74a0e3cb
Anderson Duarte Santos <Andersonduarte.24@outlook.es>
2021-10-24 21:54:30 -0500

    Se establece plantilla

---
+++ WindowsServicePlantilla/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServicePlantilla
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer { get; set; }
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            //Equibale a un minuto
+            timer.Interval = 60000;
+            timer.Elapsed += Saludar;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+
+        private void Saludar(object sender, EventArgs e) {
+            Console.WriteLine("Greetings World From Colombia)))");
+        }
+    }
+}
